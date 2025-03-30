891add8bf8fe9b4dd5dd9ca178f796402822af89
JuanLema14 <andres.lema1@udea.edu.co>
2025-03-30 15:29:15 -0500

    Elimina comentarios innecesarios en la configuración de Quasar para mejorar la claridad del código.

--- quasar.config.js
+++ quasar.config.js
@@ -63,21 +63,11 @@ export default defineConfig((ctx) => {
 
       vitePlugins: [
         ['@intlify/unplugin-vue-i18n/vite', {
-          // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
-          // compositionOnly: false,
-
-          // if you want to use named tokens in your Vue I18n messages, such as 'Hello {name}',
-          // you need to set `runtimeOnly: false`
-          // runtimeOnly: false,
-
           ssr: ctx.modeName === 'ssr',
-
-          // you need to set i18n resource including paths !
           include: [ fileURLToPath(new URL('./src/i18n', import.meta.url)) ]
-        }],
-
-        []
+        }]
       ]
+
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#devserver
